6e15a36ea8d0144054db74da9e97cbea5dba002d
Ryan Gilbert <ryangilbert7926@gmail.com>
2022-09-16 18:15:09 -0400

    feat: handle detected wallet states

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,23 @@ import {
   DynamicAuthFlow,
   DynamicContextProvider,
 } from '@dynamic-labs/sdk-react';
+import { useEffect } from 'react';
+
+export const shouldLowercaseAddress = (chain: string) =>
+  // these are standard from CAIP-2: https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-2.md
+  // see also: https://github.com/ChainAgnostic/namespaces
+  // note: no standard namespace currently exists for flow
+  ['eip155', 'flow', 'evm', 'eth'].includes(chain.toLowerCase());
+
+export const normalizeAddress = (rawAddress: string, chain: string) => {
+  const address = shouldLowercaseAddress(chain)
+    ? rawAddress.toLowerCase()
+    : rawAddress;
+  return address;
+};
+
+export const isSameAddress = (left: string, right: string, chain: string) =>
+  normalizeAddress(left, chain) === normalizeAddress(right, chain);
 
 const Home = () => {
   const {
@@ -14,6 +31,7 @@ const Home = () => {
     secondaryWallets,
     setPrimaryWallet,
     multiWalletWidgetState,
+    setMultiWalletWidgetState,
   } = useDynamicContext();
 
   const handleSignMessage = async () => {
@@ -26,6 +44,35 @@ const Home = () => {
       alert(e.message);
     }
   };
+
+  useEffect(() => {
+    if (multiWalletWidgetState === 'detected_known_secondary_wallet') {
+      primaryWallet?.connector.fetchPublicAddress().then((address) => {
+        if (!address) {
+          return;
+        }
+
+        const wallet = secondaryWallets.find((wallet) =>
+          isSameAddress(wallet.address, address, wallet.chain),
+        );
+
+        if (!wallet) {
+          return;
+        }
+
+        setMultiWalletWidgetState('idle');
+        setPrimaryWallet(wallet.id);
+      });
+    } else if (multiWalletWidgetState === 'detected_new_wallet') {
+      setMultiWalletWidgetState('awaiting_signature');
+    }
+  }, [
+    multiWalletWidgetState,
+    primaryWallet?.connector,
+    secondaryWallets,
+    setMultiWalletWidgetState,
+    setPrimaryWallet,
+  ]);
 
   if (user && primaryWallet) {
     return (
